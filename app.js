10a41cbcfcfc3b7778ecdc588b9be5f13bf2ac3b
Sarah.dev <111818953+sarahcecim@users.noreply.github.com>
2024-01-20 08:47:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = documento.querySelector('p')
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute() {
+    console.log('o botão foi clicado')
+}
